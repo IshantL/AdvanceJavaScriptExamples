74c7201efa2f3608cb6150329ee29d521907ad10
IshantL <ishantlambhate@gmail.com>
2020-01-30 16:15:46 +0530

    using call in seperate method

--- CallApplyBind/index.js
+++ CallApplyBind/index.js
@@ -14,6 +14,25 @@ let name2 = {
 
 name1.printFullName();
 //name2.printFullName(); //error
-name1.printFullName.call(name2);
+name1.printFullName.call(name2); //function borrowing
 
 
+// but the method should be outside
+
+let name3 ={
+    firstname: "Ishant",
+    lastname: "Lambhate",
+}
+
+let name4 = {
+    firstname: "Elon",
+    lastname: "Musk"
+}
+
+let printFullName = function(){
+    console.log(this.firstname + " "+ this.lastname);
+}
+   
+
+printFullName.call(name3);
+printFullName.call(name4);
